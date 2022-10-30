e94637fda3bbd109f12872f4f8acdef460037940
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-10-30 08:24:29 +0100

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,52 +1,65 @@
 #!/usr/bin/python3
-"""Unittest module for the Review Class."""
-
+"""
+Unitesting for review
+"""
 import unittest
-from datetime import datetime
-import time
-from models.review import Review
-import re
-import json
-from models.engine.file_storage import FileStorage
 import os
-from models import storage
+import pep8
+from models.review import Review
 from models.base_model import BaseModel
 
 
 class TestReview(unittest.TestCase):
 
-    """Test Cases for the Review class."""
+    @classmethod
+    def setUpClass(cls):
+        cls.Mitali = Review()
+        cls.Mitali.place_id = "SF"
+        cls.Mitali.user_id = "Test"
+        cls.Mitali.text = "Blue"
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
+    @classmethod
+    def tearDownClass(cls):
+        del cls.Mitali
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_style_check(self):
+        """
+        Tests pep8 style
+        """
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/review.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_is_sub(self):
+        self.assertTrue(issubclass(self.Mitali.__class__, BaseModel), True)
 
-    def test_8_instantiation(self):
-        """Tests instantiation of Review class."""
+    def test_checking_for_functions(self):
+        self.assertIsNotNone(Review.__doc__)
 
-        b = Review()
-        self.assertEqual(str(type(b)), "<class 'models.review.Review'>")
-        self.assertIsInstance(b, Review)
-        self.assertTrue(issubclass(type(b), BaseModel))
+    def test_has_attr(self):
+        self.assertTrue('id' in self.Mitali.__dict__)
+        self.assertTrue('created_at' in self.Mitali.__dict__)
+        self.assertTrue('updated_at' in self.Mitali.__dict__)
+        self.assertTrue('place_id' in self.Mitali.__dict__)
+        self.assertTrue('text' in self.Mitali.__dict__)
+        self.assertTrue('user_id' in self.Mitali.__dict__)
 
-    def test_8_attributes(self):
-        """Tests the attributes of Review class."""
-        attributes = storage.attributes()["Review"]
-        o = Review()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+    def test_strings(self):
+        self.assertEqual(type(self.Mitali.text), str)
+        self.assertEqual(type(self.Mitali.place_id), str)
+        self.assertEqual(type(self.Mitali.user_id), str)
+
+    def test_save(self):
+        self.Mitali.save()
+        self.assertNotEqual(self.Mitali.created_at, self.Mitali.updated_at)
+
+    def test_to_dict(self):
+        self.assertEqual('to_dict' in dir(self.Mitali), True)
+
 
 if __name__ == "__main__":
     unittest.main()
